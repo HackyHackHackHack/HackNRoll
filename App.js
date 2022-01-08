e033e0bf8b6232af0d247acc9549bd09324d310e
juliussneezer04 <vishnu.sundaresan@gmail.com>
2022-01-08 11:22:18 +0800

    mvp bug fixes

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StyleSheet, View, TextInput, Text, Button, Keyboard, ScrollView } from 'react-native';
+import { View, Text, Button, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
-import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
-import { sendMessage } from './sendMessage';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { meow } from './Meow';
+import { styles } from './styles';
 
 const Stack = createStackNavigator();
 
@@ -13,41 +13,10 @@ const SETUP_INSTRUCTIONS_1 = 'Welcome to MEOW!';
 const SETUP_INSTRUCTIONS_2 = '\nTo Start, follow the link below to authenticate this\nCallMeBot on Telegram to allow your phone to receive calls and telegram texts\n\nOnce done, come back here and click on Proceed!';
 
 export default function App() {
-    const [text, setText] = React.useState(''); 
-    const [username, setUsername] = React.useState(''); 
+    
 
     const openAuthSiteInBrowser = () => WebBrowser.openBrowserAsync('https://api2.callmebot.com/txt/login.php');
 
-    const meow = () => {
-      return (
-        <SafeAreaView>
-                <Text style={styles.subtitle}>Telegram username of recipient</Text>
-                <TextInput
-                style={styles.username}
-                textAlignVertical='top'
-                placeholder='@username'
-                value={username}
-                onChangeText={setUsername}
-                />
-                <Text style={styles.subtitle}>Message to MEOW</Text>
-                <TextInput
-                style={styles.message}
-                value={text}
-                multiline={true}
-                onChangeText={setText}
-                placeholder='Message'
-                />
-                <Button
-                  title="MEOW"
-                  color="#841584"
-                  onPress={() => {
-                    sendMessage(username, text);
-                    Keyboard.dismiss();
-                    }}
-                />
-      </SafeAreaView>);
-    };
-    
     const setup = (props) => {
       return (
       <ScrollView style={{flex: 1, marginTop:'20%'}}>
@@ -77,31 +46,3 @@ export default function App() {
       </SafeAreaProvider>
     );
 }
-
-const styles = StyleSheet.create({
-    message: {
-      marginTop: '10%',
-      height: '20%',
-      padding: '3%',
-      marginHorizontal: '2%',
-      marginBottom: '5%'
-    },
-    subtitle: {
-      marginTop: '10%',
-      fontSize: 24,
-      fontWeight: 'bold',
-      alignSelf:"flex-start",
-      textAlign: 'left',
-      marginLeft: '5%',
-      marginBottom: '5%'
-    },
-    subsubtitle: {
-      marginTop: '10%',
-      fontSize: 14,
-      fontWeight: 'bold',
-      alignSelf:"flex-start",
-      textAlign: 'left',
-      marginLeft: '5%',
-      marginBottom: '5%'
-    }
-  });
---
+++ styles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+export const styles = StyleSheet.create({
+    username:{
+      marginTop: '1%',
+      padding: '3%',
+      marginHorizontal: '2%',
+    },  
+    message: {
+        marginTop: '10%',
+        height: '20%',
+        padding: '3%',
+        marginHorizontal: '2%',
+        marginBottom: '5%'
+      },
+      subtitle: {
+        marginTop: '10%',
+        fontSize: 24,
+        fontWeight: 'bold',
+        alignSelf:"flex-start",
+        textAlign: 'left',
+        marginLeft: '5%',
+        marginBottom: '5%'
+      },
+      subsubtitle: {
+        marginTop: '10%',
+        fontSize: 14,
+        fontWeight: 'bold',
+        alignSelf:"flex-start",
+        textAlign: 'left',
+        marginLeft: '5%',
+        marginBottom: '5%'
+      }
+    });
+  
